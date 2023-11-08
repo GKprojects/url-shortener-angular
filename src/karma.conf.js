cd266237136ee0f2aa56677f4f1222221175bd8b
GokulGk <94182802+GKprojects@users.noreply.github.com>
2023-11-08 21:23:01 +0530

    Update karma.conf.js

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,21 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+// karma.conf.js
+
+module.exports = function(config) {
+  config.set({
+    // ... other Karma configuration settings ...
+
+    reporters: ['progress', 'xml'],
+
+    // Add the xmlReporter configuration
+    xmlReporter: {
+      outputFile: '../coverage/test-results.xml' // Specify the desired output file name and path
+    },
+
+    // ... other Karma configuration settings ...
+  });
+};
 
 module.exports = function (config) {
   config.set({
@@ -9,7 +25,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-xml-reporter'),
+      // require('karma-xml-reporter'),
       require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
@@ -18,15 +34,13 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    // coverageIstanbulReporter: {
-    //   dir: require('path').join(__dirname, '../coverage'),
-    //   reports: ['html', 'lcovonly', 'text-summary'],
-    //   fixWebpackSourcePaths: true
-    // },
-    xmlReporter: {
-      dir: require('path').join(__dirname, '../coverage/test-results.xml'),
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'xml'],
+  
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
